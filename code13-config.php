7f5bac18ab26f5cacc817f4d4355b9054ed64c37
codegarden13 <code@xviz.de>
2025-05-10 21:33:33 +0200

    First Commit

---
+++ code13-config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Code13 Config
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     code13-config
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Code13_Config
+ */
+
+// Your code starts here.
